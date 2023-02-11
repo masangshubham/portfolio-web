bb41b241c9b9296df0779d74a46a7967d4f6cf36
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2023-02-11 19:24:36 +0530

    fix projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,6 +18,7 @@ const Projects = () => (
               <Hr />
             </TitleContent>
             <CardInfo className="card-info">{p.description}</CardInfo>
+            <br />
             <div>
               <TitleContent>Stack</TitleContent>
               <TagList>
