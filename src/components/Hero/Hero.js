9bb7443870aff7d41b37ec6bf5411cfdd228075f
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2023-02-11 18:42:21 +0530

    add resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,15 +16,19 @@ const Hero = (props) => (
         <br />
         Good to see you.
       </SectionTitle>
-      I am a full-stack web developer based in Pune, India.
-      Currently pursuing my B-tech in Information Technology.
-      I've worked on various web projects on technologies ranging from HTML and CSS to Next and Firebase. Here's my resume to find out more...
-
-      <SectionText>
-        
-      </SectionText>
+      I am a full-stack web developer based in Pune, India. Currently pursuing
+      my B-tech in Information Technology. I've worked on various web projects
+      on technologies ranging from HTML and CSS to Next and Firebase. Here's my
+      resume to find out more...
+      <SectionText></SectionText>
       <Button>
-        <a href="https://masangshubham.github.io/Portfolio/ShubhamMasang.pdf" target='_blank' style={{color: 'inherit'}}>Resume</a>
+        <a
+          href="https://masangshubham.github.io/portfolio-web/blob/main/public/ShubhamMasang.pdf"
+          target="_blank"
+          style={{ color: "inherit" }}
+        >
+          Resume
+        </a>
       </Button>
     </LeftSection>
   </Section>
