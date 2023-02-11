250be5fb3d6043c8515218cfc9c9c07ec67d8bbb
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2023-02-11 19:22:45 +0530

    add tech stack to projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,14 +4,15 @@ export const projects = [
     description:
       "Blogging platform inspired by dev.to. Implemented SSR and ISR for content to make it search engine friendly and let bots crawl the content. Dynamic and highly interactive with changes visible in real time.",
     image: "/images/appSS.png",
-    tags: ["Next", "react", "Firebase", "OAuth"],
+    tags: ["Next", "React", "Firebase", "OAuth"],
     source: "https://github.com/masangshubham/blog-states",
     visit: "https://blogstates.vercel.app/",
     id: 0,
   },
   {
     title: "Quoted",
-    description:"A web application that generates random quotes which can be shared on Twitter or downloaded as images. User can also add their quotes to be shared or downloaded",
+    description:
+      "A web application that generates random quotes which can be shared on Twitter or downloaded as images. User can also add their quotes to be shared or downloaded",
     image: "/images/quotedss.png",
     tags: ["React", "Tailwind"],
     source: "https://github.com/masangshubham/Quoted",
@@ -23,7 +24,7 @@ export const projects = [
     description:
       "Made using Node.js, Ejs and MongoDB as a database user can create and delete a task and the changes are reflected on the site and database.",
     image: "/images/todoss.png",
-    tags: ["React", "WebRTC"],
+    tags: ["Node", "MongoDB"],
     source: "https://github.com/masangshubham/To-Do-List",
     visit: "https://todolist-eld8.onrender.com/",
     id: 2,
@@ -33,9 +34,10 @@ export const projects = [
     description:
       "About Clone of Google Keep providing basic functionality of creating notes and deleting them.",
     image: "/images/keeperss.png",
-    tags: ["React", "ChatEngine", "Firebase"],
+    tags: ["React", "Material UI"],
     source: "https://github.com/masangshubham/Keeper",
-    visit: "https://639e0cbb18290c50a9ff4953--friendly-beijinho-8e84af.netlify.app/",
+    visit:
+      "https://639e0cbb18290c50a9ff4953--friendly-beijinho-8e84af.netlify.app/",
     id: 3,
   },
 ];
